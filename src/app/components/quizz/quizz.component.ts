12d5cb2931f0ecbaa90f4a9a5ffaa270cade6319
BrunoHenriqueOliveira <bruno.oliveira.tc@outlook.com>
2025-03-09 19:14:35 -0300

    refactor: refatoração em logica para buscar e exibir perguntas

--- src/app/components/quizz/quizz.component.ts
+++ src/app/components/quizz/quizz.component.ts
@@ -1,70 +1,58 @@
-import { Component } from '@angular/core';
-import {NgForOf, NgIf} from '@angular/common';
-import quizz_questions from "../../../assets/data/quizz_questions.json"
+import { Component, OnInit } from '@angular/core';
+import { NgForOf, NgIf } from '@angular/common';
+import quizz_questions from "../../../assets/data/quizz_questions.json";
 
 @Component({
   selector: 'app-quizz',
-  imports: [
-    NgIf,
-    NgForOf
-  ],
+  standalone: true, // Corrigido para standalone components
+  imports: [NgIf, NgForOf],
   templateUrl: './quizz.component.html',
   styleUrl: './quizz.component.css'
 })
-export class QuizzComponent {
-  title:string = "";
+export class QuizzComponent implements OnInit {
+  title: string = "";
+  questions: any[] = [];
+  questionSelected: any = null;
+  answers: string[] = [];
+  answerSelected: string = "";
 
-  questions:any = "";
-  questionSelected:any = "";
-  answers:string[] = [];
-  answerSelected:string = "";
+  questionId: number = 0;
+  questionMaxId: number = 0;
+  finished: boolean = false;
 
-  questionId:number = 0;
-  questionMaxId:number = 0;
-
-  finished:boolean = false;
-
-  ngOnInit() {
+  ngOnInit(): void {
     if (quizz_questions) {
-      this.finished = false
       this.title = quizz_questions.title;
-
       this.questions = quizz_questions.questions;
-      this.questionSelected = this.questions[this.questionId];
-
-      this.questionId = 0
       this.questionMaxId = this.questions.length;
+      this.questionSelected = this.questions[this.questionId]; // Definir após questionMaxId
     }
   }
 
-  playerChoose(val:string) {
-    this.answers.push(val);
-    this.nextStep()
+  playerChoose(answer: string): void {
+    this.answers.push(answer);
+    this.nextStep();
   }
 
-  async nextStep(){
-    this.questionId += 1;
+  private nextStep(): void {
+    this.questionId++;
 
-    if (this.questionMaxId > this.questionId) {
-      this.questionSelected = this.questions[this.questionId]
+    if (this.questionId < this.questionMaxId) {
+      this.questionSelected = this.questions[this.questionId];
     } else {
-      const finalAnswer:string = await this.checkResult(this.answers)
       this.finished = true;
-      this.answerSelected = quizz_questions.results[finalAnswer as keyof typeof quizz_questions.results];
+      this.calculateResult();
     }
   }
 
-  async checkResult(answer:string[]) {
-    const result = answer.reduce((previous, current, i, arr) => {
-      if (
-        arr.filter(item => item === previous).length >
-        arr.filter(item => item === current).length
-      ){
-        return previous;
-      } else {
-        return current
-      }
-    })
-    return result;
+  private calculateResult(): void {
+    const finalAnswer = this.getMostFrequentAnswer(this.answers);
+    this.answerSelected = quizz_questions.results[finalAnswer as keyof typeof quizz_questions.results];
+  }
+
+  private getMostFrequentAnswer(answers: string[]): string {
+    return answers.reduce((prev, curr) =>
+      answers.filter(item => item === prev).length >= answers.filter(item => item === curr).length ? prev : curr
+    );
   }
 }
